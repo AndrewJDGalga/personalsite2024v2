9b4ccfb7c324a1c1e0f358331f9a5eab01206ee3
Andrew DGalga <ex.spake@gmail.com>
2024-07-28 20:43:22 -0500

    skills hide-reveal button logic.

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+const btnsSkills = document.getElementsByClassName('skills_button');
+function revealFollowing(e) { e.target.nextElementSibling.classList.toggle('hidden'); }
+for(let i = 0; i < btnsSkills.length; i++) btnsSkills[i].onclick = revealFollowing;
+
 const projectsHook = document.getElementById('projects_hook');
